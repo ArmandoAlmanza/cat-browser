1b6ea63eefc42ce46ad02c80df623a0bafbc6bc8
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-20 11:31:14 -0600

    ommit grid component for reqs

--- src/components/CatlistGrid.tsx
+++ src/components/CatlistGrid.tsx
@@ -14,11 +14,11 @@ const CatlistGrid = () => {
     return (
         <div className="max-w-4xl mx-auto flex flex-col content-center gap-5">
             <h1 className="text-4xl text-center font-semibold">Ira muchos gatos</h1>
-            <div className="grid grid-cols-2 place-content-center place-items-center border border-gray-200 rounded-2xl gap-4 md:grid-cols-3 p-5">
+            <div className="grid grid-cols-2 place-content-center place-items-center border border-gray-200 rounded-2xl gap-4 md:grid-cols-3 py-5 px-2">
                 {catImages
                     ? catImages.map((catImage, i) => (
                           <img
-                              className="min-h-48 max-h-48 max-w-32 md:min-w-48 md:max-w-48"
+                              className="min-h-48 max-h-48 w-11/12 max-w-fit md:min-w-48 md:max-w-48"
                               key={i}
                               src={catImage.url}
                               alt="image cat"
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { CatImageType, mockCatImage } from "../interfaces/CatInterface";
+import { getRandomCat } from "../api/axiosResponse";
+
+const LandingPage = () => {
+    const [randomCatImage, setRandomCatImage] =
+        useState<CatImageType>(mockCatImage);
+
+    const randomCat = async () => {
+        getRandomCat().then((data) => {
+            setRandomCatImage(data.data[0]);
+        });
+    };
+    return (
+        <div className="flex flex-col gap-5 items-center content-center p-3 mt-4 md:h-full">
+            <h2 className="text-center text-3xl font-semibold">Welcome!</h2>
+            <p className="text-center ">
+                Fell free to get a random image of a cat
+            </p>
+            <button
+                className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mx-auto"
+                onClick={randomCat}
+            >
+                Get Random Cat!!
+            </button>
+            {randomCatImage !== mockCatImage ? (
+                <img src={randomCatImage.url} alt="random cat image" />
+            ) : (
+                ""
+            )}
+        </div>
+    );
+};
+
+export default LandingPage;
