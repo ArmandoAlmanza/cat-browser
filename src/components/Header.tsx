dec587afdb1bc0d2df43090c7cf833dc0ff1998c
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-20 12:23:33 -0600

    add a hook for screen size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,27 @@
 import { FaBarsStaggered } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import useScreenSize from "../hooks/useScreenSize";
 
 const Header = () => {
+    const screenSize = useScreenSize();
     return (
-        <header className="flex flex-row justify-between p-4 border-b border-b-gray-200 sticky content-center items-center md:flex-col md:justify-evenly md:border-b-0 md:border-r md:border-r-gray-200 bg-[#FDFDFD] md:w-64">
-            <h1 className="text-3xl font-semibold">
-                Cat <span className="text-stone-300">Browser</span>
-            </h1>
-            <Link className="outline-none px-3 text-2xl block md:hidden" to={"/cat-list"}>
-                <FaBarsStaggered />
-            </Link>
-			<Navbar />
-        </header>
+        <>
+            <header className="flex flex-row justify-between p-4 border-b border-b-gray-200 sticky content-center items-center md:flex-col md:justify-evenly md:border-b-0 md:border-r md:border-r-gray-200 bg-[#FDFDFD] md:w-64">
+                <h1 className="text-3xl font-semibold">
+                    Cat <span className="text-stone-300">Browser</span>
+                </h1>
+                <Link
+                    className="outline-none px-3 text-2xl block md:hidden"
+                    to={"/cat-list"}
+                >
+                    <FaBarsStaggered />
+                </Link>
+                <Navbar />
+                {screenSize.width >= 768 ? <Footer /> : ""}
+            </header>
+        </>
     );
 };
 
