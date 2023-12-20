5b051a7da40cd2ca443272640488b80dea357980
Armando Almanza <69016557+ArmandoAlmanza@users.noreply.github.com>
2023-12-20 13:52:36 -0600

    forget to erase the test jajaja

--- src/api/axiosResponse.ts
+++ src/api/axiosResponse.ts
@@ -1,15 +1,5 @@
 import axios from "./axios";
 
-export const getCatsImagesByBreed = async (
-    breed_id: string,
-    amount: number
-) => {
-    const res = await axios(
-        "/images/search?breed_ids=" + breed_id + "&limit=" + amount
-    );
-    return res.data;
-};
-
 export const getCatsGrid = async () => axios.get("/images/search?limit=12");
 
 export const getRandomCat = async () =>
