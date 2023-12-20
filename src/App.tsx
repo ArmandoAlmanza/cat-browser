371d454398428b8e02877c8355ff0c62d49301eb
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-20 11:31:00 -0600

    migrate routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Outlet } from "react-router-dom";
 
 function App() {
     return (
-        <>
+        <main className="flex flex-col content-center md:flex-row md:h-screen md:w-full">
             <Header />
-            <div className="md:flex-1 px-8 py-32">
+            <div className="md:flex-1 md:px-8 md:py-32">
                 <Outlet />
             </div>
-        </>
+        </main>
     );
 }
 
