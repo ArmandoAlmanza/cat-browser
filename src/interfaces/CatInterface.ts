d1dfdd7b32030aa309bfbc96542ac085c0989c57
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-20 11:31:31 -0600

    add mock data

--- src/interfaces/CatInterface.ts
+++ src/interfaces/CatInterface.ts
@@ -1,6 +1,11 @@
 export interface CatImageType {
     id: string;
     url: string;
-    width: number;
-    height: number;
+    width?: number;
+    height?: number;
 }
+
+export const mockCatImage = {
+    id: "",
+    url: "",
+};
