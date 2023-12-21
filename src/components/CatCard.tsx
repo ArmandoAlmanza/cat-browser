0222b8b20a284b8ef25b080ccb60ba1bdc76f883
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-21 13:45:03 -0600

    add the useEffect for info

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+import { CatBreed } from "../interfaces/CatInterface";
+import { getBreedData } from "../api/axiosResponse";
+
 interface CatProps {
     catId: string;
 }
 
 const CatCard = ({ catId }: CatProps) => {
+    const [catBreed, setCatBreed] = useState<CatBreed>();
+
+    useEffect(() => {
+        getBreedData(catId).then((data) => {
+            setCatBreed(data.data);
+            console.log(data.data);
+        });
+    }, [catId]);
     return (
         <article className="flex flex-col justify-center">
             <h1>{catId}</h1>
