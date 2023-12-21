6840811cdcd324c5909e58566ee64162cc64b8ce
Armando Almanza <armando.s.almanza@gmail.com>
2023-12-21 13:44:52 -0600

    add url to get cat info

--- src/api/axiosResponse.ts
+++ src/api/axiosResponse.ts
@@ -5,3 +5,5 @@ export const getCatsGrid = async () => axios.get("/images/search?limit=12");
 export const getRandomCat = async () => axios.get("/images/search");
 
 export const getBreeds = async () => axios.get("/breeds/");
+
+export const getBreedData = async (id: string) => axios.get("/breeds/" + id);
